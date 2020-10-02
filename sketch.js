dd2598075b1cb0c518274a90a17684246fd4e34d
adithyanri <72028547+adithyanri@users.noreply.github.com>
2020-10-02 22:13:01 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,58 @@
+
+
+
+var bird,birdImage;
+var pipes = [];
+
+function preload(){
+  
+  birdImage=loadImage("flappy.png")
+  
+}
+
+
+function setup() {
+  createCanvas(640, 480);
+  bird = new Bird();
+  pipes.push(new Pipe());
+  
+      
+}
+
+function draw() {
+  background(0);
+  
+  fill("red");
+  text("press space to jump",200,200);
+  
+  
+  for (var i = pipes.length-1; i >= 0; i--) {
+    pipes[i].show();
+    pipes[i].update();
+
+    if (pipes[i].hits(bird)) {
+      console.log("HIT");
+      bird.velocityY=0;
+      bird.velocityX=0;
+    
+    }
+
+    if (pipes[i].offscreen()) {
+      pipes.splice(i, 1);
+    }
+  }
+
+  bird.update();
+  bird.show();
+
+  if (frameCount % 75 == 0) {
+    pipes.push(new Pipe());
+  }
+}
+
+function keyPressed() {
+  if (key == ' ') {
+    bird.up();
+    //console.log("SPACE");
+  }
+}
